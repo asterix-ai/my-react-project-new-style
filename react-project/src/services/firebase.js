200ae66bc49c2325d3d144ec2a427875d4505d6a
asterix-ai <asterixaidevelopment@gmail.com>
2025-10-19 18:51:18 +0300

    Add react-project/src/services/firebase.js

---
+++ react-project/src/services/firebase.js
@@ -0,0 +1,5 @@
+import { projectId } from '../contexts/FirebaseContext';
+
+// This file can be used to export the projectId for services that need it
+// and potentially other non-context-dependent Firebase utilities.
+export { projectId };
